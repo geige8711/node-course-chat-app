298fd2d6cf88b0179e31d113b99dceed9c758874
Yue He <yuehe@Yues-Mac-mini.gateway>
2018-09-04 08:42:06 +1000

    initialize the node-chat-app

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+let app = express();
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+
+console.log(__dirname + '/../public');
+console.log(publicPath);
+
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`);
+});
