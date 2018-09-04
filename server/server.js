b68561ad8019b9ed22ec77cc3bb0dd1651c55600
Yue He <yuehe@Yues-Mac-mini.gateway>
2018-09-04 22:43:18 +1000

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -23,22 +23,27 @@ io.on('connection', (socket) => {
     socket.on('disconnect', ()=>{
         console.log('user is not connected');
     });
-    socket.emit('newEmail',{
-        from: 'mike@example.com',
-        text: 'Hey. What is going on.',
-        createdAt: 123
-    });
-    socket.on('createEmail', (newEmail) => {
-        console.log('createEmail', newEmail);
-    });
+    // socket.emit('newEmail',{
+    //     from: 'mike@example.com',
+    //     text: 'Hey. What is going on.',
+    //     createdAt: 123
+    // });
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail);
+    // });
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'See you then',
-        createdAt: 123123
-    });
+    // socket.emit('newMessage', {
+    //     from: 'John',
+    //     text: 'See you then',
+    //     createdAt: 123123
+    // });
 });
 
 
