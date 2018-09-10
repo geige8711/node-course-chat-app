e5e4afd3e204ca216e1621f95c314cbcf88b3edf
He Yi <hi.he.aksu@gmail.com>
2018-09-10 12:38:19 +1000

    Add css for chat page

--- public/JS/index.js
+++ public/JS/index.js
@@ -33,12 +33,13 @@ socket.on('connect', () => {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+    let messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        jQuery('[name=message]').val('')
     })
 });
 let locationButton = jQuery('#send-location');
@@ -46,13 +47,17 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
         // console.log(position);
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
+        locationButton.removeAttr('disabled').text('Send location');
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
