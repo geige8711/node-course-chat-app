4f2b5ab055c39e423809aacf7f22566a0e3578d9
He Yi <hi.he.aksu@gmail.com>
2018-09-20 21:09:52 +1000

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -53,8 +53,10 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        let user = users.getUser(socket.id);
+        if(user && isRealString(message.text)){
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
         callback('this is from server!');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
@@ -64,7 +66,10 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+        let user = users.getUser(socket.id);
+        if(user){
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+        }
     });
 
     // socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
